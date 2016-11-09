6135f4ea82feef75c590fe1d82eff1521eba0e1b
Agustín Aceves Osuna <Agustin@MacBookProAAO.local>
2016-11-08 19:18:39 -0600

    Por Fin

--- JuegoDeMemoria/ViewController.swift
+++ JuegoDeMemoria/ViewController.swift
@@ -13,6 +13,24 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
+        for index in 0...100 {
+            
+            if index > 0 {
+                if index % 5 == 0 {
+                    print ("\(index) Bingo!!!")
+                }
+                if index % 2 == 0 {
+                    print ("\(index) par!!!")}
+                else {
+                    print ("\(index) inpar!!!")
+                }
+            }
+            if index >= 30 && index <= 40 {
+                print ("\(index) Viva Swift!!!")
+            }
+        }
+
     }
 
     override func didReceiveMemoryWarning() {
